42ca29ca33b50add552864184188b1c757432740
Felippe <raposo.felippe@gmail.com>
2013-11-21 14:26:33 -0200

    Refactoring game class

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,28 +1,47 @@
 module Codebreaker
   class Game
+
     def initialize(output)
       @output = output
     end
+
     def start(secret)
       @secret = secret
       @output.puts "Welcome to Codebreaker!"
       @output.puts "Enter guess: "
     end
+
     def guess(guess)
-      mark =  ""
-      (0..3).each do |index|
-        exact_match?(guess, index) ? mark << '+' : nil
+      marker = Marker.new(@secret, guess)
+      @output.puts '+' * marker.exact_match_count +
+                   '-' * marker.number_match_count
+    end
+  end
+
+  class Marker
+
+    def initialize(secret, guess)
+      @secret, @guess = secret, guess
+    end
+
+    def exact_match?(index)
+      @guess[index] == @secret[index]
+    end
+
+    def number_match?(index)
+      @secret.include?(@guess[index]) && !exact_match?(index)
+    end
+
+    def exact_match_count()
+      (0..3).inject(0) do |count,index|
+        count + (exact_match?(index) ? 1 : 0)
       end
-      (0..3).each do |index|
-        number_match?(guess, index) ? mark << '-' : nil
+    end
+
+    def number_match_count()
+      (0..3).inject(0) do |count,index|
+        count + (number_match?(index) ? 1 : 0)
       end
-      @output.puts mark
-   end
-   def exact_match?(guess, index)
-     guess[index] == @secret[index]
-   end
-   def number_match?(guess, index)
-     @secret.include?(guess[index]) && !exact_match?(guess, index)
-   end
+    end
   end
 end
