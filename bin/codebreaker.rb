dd0b07ebf1fd16c80f5bc5864448fe7c6fc4103f
Felippe <raposo.felippe@gmail.com>
2013-11-21 12:51:00 -0200

    'Initial' commit, chapter 8

---
+++ bin/codebreaker.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'codebreaker'
+
+game = Codebreaker::Game.new(STDOUT)
+game.start
---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game)   { Game.new(output) }
+
+    describe "#start" do
+      it "sends a welcome message" do
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+        game.start('1234')
+      end
+      it "prompts for the first guess" do
+        output.should_receive(:puts).with('Enter guess: ')
+        game.start('1234')
+      end
+    end
+
+    describe "#guess" do
+      context "with no matches" do
+        it "sends a mark with ''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context "with on number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context "with two number matches" do
+        it "sends a markk with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+
+      context "with one exact match" do
+        it "sends a mark with '+'"do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+      context "with one number match and one exact match" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
+    end
+  end
+end
