ba11216f767179e81c53051c1cb0d96eebcc96a1
Felippe <raposo.felippe@gmail.com>
2013-11-21 14:35:36 -0200

    Removes marker class from game.rb and creates its own file

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,6 +1,5 @@
 module Codebreaker
   class Game
-
     def initialize(output)
       @output = output
     end
@@ -15,33 +14,6 @@ module Codebreaker
       marker = Marker.new(@secret, guess)
       @output.puts '+' * marker.exact_match_count +
                    '-' * marker.number_match_count
-    end
-  end
-
-  class Marker
-
-    def initialize(secret, guess)
-      @secret, @guess = secret, guess
-    end
-
-    def exact_match?(index)
-      @guess[index] == @secret[index]
-    end
-
-    def number_match?(index)
-      @secret.include?(@guess[index]) && !exact_match?(index)
-    end
-
-    def exact_match_count()
-      (0..3).inject(0) do |count,index|
-        count + (exact_match?(index) ? 1 : 0)
-      end
-    end
-
-    def number_match_count()
-      (0..3).inject(0) do |count,index|
-        count + (number_match?(index) ? 1 : 0)
-      end
     end
   end
 end
